64ec23a449eb34e9e98044e3501b1ebf8a4c9c9a
Chris <github@dreiundsiebzig.eu>
2023-10-02 16:51:51 +0200

    Update 404.php
    Cleanup to HTML 404 ERROR document

--- 404.php
+++ 404.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 <article id="post-0" class="post not-found">
 <header class="header">
-<h1 class="entry-title" itemprop="name"><?php esc_html_e( 'Not Found', 'faktor73' ); ?></h1>
+<h1 class="entry-title" itemprop="name" style="text-align: center;"><?php esc_html_e( 'Error 404: Page Not Found.', 'faktor73' ); ?></h1>
+<p style="text-align: center;">Sorry, you've followed an outdated link. If you believe this is a mistake, get in <a style="color: #363636; text-decoration: none; border-bottom: 1px solid #363636" href="/kontakt">contact</a> with me.</p>
+<style>
+	#header, .basel {display: none;}	
+</style>
 </header>
 <div class="entry-content" itemprop="mainContentOfPage">
-<p><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'faktor73' ); ?></p>
-<?php get_search_form(); ?>
 </div>
 </article>
 <?php get_footer(); ?>
